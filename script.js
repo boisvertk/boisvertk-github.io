c6c2e507ea2daa349e5e9578c15e49eb32432401
Kala Boisvert <kalaaboisvert@gmail.com>
2018-04-23 15:30:30 -0400

    changes

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-// Get the modal
-var modal = document.getElementById('myModal');
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
+// Get the navbar
+var navbar = document.getElementById("navbar");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-    modal.style.display = "none";
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
