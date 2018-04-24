42c9824d13dd33933c201bdbd9e52b8dd27d6a0b
Kala Boisvert <kalaaboisvert@gmail.com>
2018-04-24 14:07:58 -0400

    navbar slides

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// // When the user scrolls the page, execute myFunction 
+$(document).ready(function(){
+    $('.hiding').slideDown(1000);
+      });
+
+
 // window.onscroll = function() {myFunction()};
 
-// // Get the navbar
-// var navbar = document.getElementById("navbar");
+// var navigationbar = document.getElementById("navigationbar");
+// var sticky = navigationbar.offsetTop;
 
-// // Get the offset position of the navbar
-// var sticky = navbar.offsetTop;
-
-// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 // function myFunction() {
 //   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
+//     navigationbar.classList.add("sticky")
 //   } else {
-//     navbar.classList.remove("sticky");
+//     navigationbar.classList.remove("sticky");
 //   }
 // }
 
@@ -36,3 +36,23 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
     modal.style.display = "none";
 }
+// // Get the modal
+// var modal = document.getElementById('myModal');
+
+// // Get the image and insert it inside the modal - use its "alt" text as a caption
+// var img = document.getElementById('myImg2');
+// var modalImg = document.getElementById("img02");
+// var captionText = document.getElementById("caption");
+// img.onclick = function(){
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+//     captionText.innerHTML = this.alt;
+// }
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() { 
+//     modal.style.display = "none";
+// }
